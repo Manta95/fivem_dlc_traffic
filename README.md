c213552430e9a24882b672be40af9686dfb7dc18
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2025-05-13 13:35:58 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# fivem_dlc_traffic
-Add DLC Cars in traffic for FiveM
+
+# Installation
+- Downloard the latest version of the code
+- Put it in your `ressource` folder and ensure it in your `server.cfg`
+- Enjoy
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+author "L'kid"
+description "Skysder's Enhanced Traffic Experience: Lite Edition for FiveM"
+version "5.0"
+
+dependencies {
+    "/gameBuild:3258",
+}
+
+this_is_a_map "yes"
+
+files {
+	"popgroups.ymt",
+	"popcycle.dat",
+}
+
+data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "popgroups.ymt"
+data_file "POPSCHED_FILE" "popcycle.dat"
